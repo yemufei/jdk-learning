f05d0e042f8340ef26e1fd0c72bcecfc75ce95d1
yemufei <yemufei@126.com>
2022-08-21 06:15:29 +0800

    a

--- src/main/java/com/learning/bo/bo.java
+++
@@ -1,9 +0,0 @@
-package com.learning.bo;
-
-/**
- * @author: mufei
- * @date: 2022/8/21 05:47
- * @Description:
- */
-public class bo {
-}
---
+++ src/test/java/Test.java
@@ -0,0 +1,29 @@
+import com.learning.bo.Student;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author: mufei
+ * @date: 2022/8/21 05:54
+ * @Description:
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        Student stuA = new Student("张三", "20");
+        Student stuB = new Student("张三", "20");
+        System.out.println(stuA.equals(stuB));                   // true
+        System.out.println(stuA == stuB);                        // false
+        System.out.println(stuA.hashCode() == stuB.hashCode());  // true
+
+        Set<Student> set = new HashSet<>();
+        set.add(stuA);
+        set.add(stuB);
+        set.add(null);
+        System.out.println(set.size());
+
+    }
+
+}
